7978186e99df2d0c6cc9bc5d2251b92a2d331ec1
Lachlan.McDermott <Lachlan.McDermott@GMR-124-2-1>
2022-11-20 20:20:30 -0800

    Add project files.

---
+++ GMRSchemeTalk/Program.cs
@@ -0,0 +1,39 @@
+﻿using GMRSchemeTalk;
+using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<int>> arr = PascalsTriangle(5);
+        }
+
+        public static List<List<int>> PascalsTriangle(int n)
+        {
+            List<List<int>> ret = new List<List<int>>();
+           
+            // n == 5
+            for (int i = 1; i <= n; i++)
+            {
+                if (i == 1)
+                {
+                    ret.Add(new List<int> { 1 });
+                }
+
+                List<int> temp = new List<int>();
+                temp.Add(1);
+                for (int y = 1; y < i; y++)
+                {
+                    temp.Add(ret[i - 1][y] + ret[i - 1][y - 1]);
+                }
+                temp.Add(1);
+                ret.Add(temp);
+            }
+            return ret;
+        }
+    }
+}
---
+++ GMRSchemeTalk/node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GMRSchemeTalk
+{
+    internal class Node<T>
+    {
+        public int Val = 1;
+        public int Height = 1;
+        public Node<T> Right = new Node<T>(default, default, 0);
+        public Node<T> Left = new Node<T>(default, default, 0);
+
+        public Node(Node<T> right, Node<T> left, int height)
+        {
+            Height = height;
+            Right = right;
+            Left = left;
+        }
+
+    }
+}
